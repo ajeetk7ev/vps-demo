57531304a2e84cba71d3e963c411948f651bd205
ajeetk7ev <ajeetk8568@gmail.com>
2025-12-31 20:54:43 +0530

    external IP added

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,6 @@ app.get("/health", (req, res) => {
    res.send("I am working fine")
 })
 
-app.listen(3000,() => {
+app.listen(3000,"0.0.0.0",() => {
     console.log("Server is running at port 3000")
 })
