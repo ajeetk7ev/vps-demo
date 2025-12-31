135f4e6b809ffe1516c201649efccd136c5e764c
ajeetk7ev <ajeetk8568@gmail.com>
2025-12-31 20:36:24 +0530

    Create backend

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/health", (req, res) => {
+   res.send("I am working fine")
+})
+
+app.listen(3000,() => {
+    console.log("Server is running at port 3000")
+})
