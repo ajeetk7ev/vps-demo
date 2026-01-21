e05752c3044bcc9488154fcdec36cfc02f792f8d
Ajeet Kumar <134052500+ajeetk7ev@users.noreply.github.com>
2026-01-21 20:29:56 +0530

    Add GET endpoint for retrieving users
    Fix syntax errors in the GET users API route.

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,10 @@ app.post("/api/users",(req, res) => {
    console.log("This is api for user creation");
 })
 
+app.get("/api/users, (req, res) => {
+        console.log("This is the api for get users);
+})
+
 app.listen(3000,"0.0.0.0",() => {
     console.log("Server is running at port 3000")
 })
