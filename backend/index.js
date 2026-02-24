3cec6dfdd0a0f95a758794697d84b0e66c1879ab
Ajeet Kumar <134052500+ajeetk7ev@users.noreply.github.com>
2026-02-24 17:39:27 +0530

    Fix response format for user creation API

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,9 @@ app.post("/api/users",(req, res) => {
    console.log("This is api for user creation");
 })
 
+{"success":true,"data":{"success":false,"reason":"no_devices"}}%         
+{"success":true,"data":{"success":false,"reason":"no_devices"}}%         
+
 app.listen(3000,"0.0.0.0",() => {
     console.log("Server is running at port 3000")
 })
